d5b6a49b8458009bc4932a1a3ca420bdb1e20a0a
Pavel Pavlov <resolversoft@gmail.com>
2021-03-29 14:35:54 +0300

    added addAnyValue

---
+++ addAnyValue/addAnyValue.js
@@ -0,0 +1,14 @@
+const arr = [1, 3 ,5 ,6 ,7];
+
+
+    let sum = (array) =>{
+
+        let total = 0;
+
+        array.forEach(index => total += index);
+        
+        return console.log(total)
+    }
+
+    
+    sum(arr);
