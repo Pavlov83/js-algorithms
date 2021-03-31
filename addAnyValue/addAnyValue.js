c7d94da19361635d3a685ff996b0a6f214a8c279
Pavel Pavlov <resolversoft@gmail.com>
2021-03-31 10:51:46 +0300

    addTwoDigits and addBorder

--- addAnyValue/addAnyValue.js
+++ addAnyValue/addAnyValue.js
@@ -6,7 +6,6 @@ const arr = [1, 3 ,5 ,6 ,7];
         let total = 0;
 
         array.forEach(index => total += index);
-        
         return console.log(total)
     }
 
---
+++ addTwoDigits/addTwoDigits.js
@@ -0,0 +1,12 @@
+
+const arr = [2,3]
+
+function addTwoDigits(n){
+   
+
+    return arr[0] + arr[1]
+    
+      
+}
+
+console.log(addTwoDigits(arr));
