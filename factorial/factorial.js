4bd4f9a0c525fc4c4a38518e130549b3ebdec6d8
p <p@ps-MacBook-Pro.local>
2021-04-05 22:42:01 +0300

    added almost increasing sequence

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,12 +1,29 @@
 //factorialize number with recursion
 
-function factorializeRec(num){
-    if(num < 0) return -1;
+// function factorializeRec(num){
+//     if(num < 0) return -1;
 
-    else if(num === 0){
-        return 1
-    }else{
-        return(num * factorializeRec(num - 1))
+//     else if(num === 0){
+//         return 1
+//     }else{
+//         return(num * factorializeRec(num - 1))
+//     }
+
+// }
+
+//factorialize with while
+
+function factorializeWhile(num){
+    let result = num;
+
+    if(num < 0 || num == 1)return 1;
+
+    while(num > 1){
+        num--;
+        result *= num;
     }
 
+    return result
 }
+
+console.log(factorializeWhile(40))
